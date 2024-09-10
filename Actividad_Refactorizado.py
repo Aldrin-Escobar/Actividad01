a034bd10572d8184ab7b911397bcd5446d097838
Codigo <72879174@continental.edu.pe>
2024-09-10 16:44:03 -0500

    Se añadio los metodos Sumar y Restar

--- Actividad_Refactorizado.py
+++ Actividad_Refactorizado.py
@@ -1,6 +1,10 @@
 # Se Elimina 'if y != 0 else x / y' -> Debio a que si y == 0, se realizará una división con denominador
 # 0 y nos arrogará un error.
 class OperacionMatematica:
-    def Multipicacion(self, Multiplicando, Multiplicador):
-        return Multiplicando*Multiplicador
+    def Multipicacion(self, multiplicando, multiplicador):
+        return multiplicando * multiplicador
 
+    def Sumar(self, sumandoX, sumandoY):
+        return sumandoX + sumandoY
+    def Restar(self, minuendo, sustraendo):
+        return minuendo - sustraendo
