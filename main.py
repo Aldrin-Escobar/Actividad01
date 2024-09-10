a4a3d59791432f8ad1edcfb7a6490ca33f9e4026
Codigo <72879174@continental.edu.pe>
2024-09-10 16:44:55 -0500

    Se añadio las pruebas de Sumar y Restar en el archivo main.py

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ if __name__ == "__main__":
     multiplicando = float(input("Ingresar multiplicando: "))
     multiplicador = float(input("Ingresar multiplicando: "))
     resultado = operacion.Multipicacion(multiplicando,multiplicador)
-    print(f"{multiplicando} * {multiplicador} = {resultado}")
+    print(f"Multplicacion -> {multiplicando} * {multiplicador} = {resultado}")
+
+    sumandoA = float(input("Ingresar primer sumando: "))
+    sumandoB = float(input("Ingresar segundo sumando: "))
+    resultado = operacion.Sumar(sumandoA, sumandoB)
+    print(f"Suma -> {sumandoA} + {sumandoB} = {resultado}")
+
+    minuendo = float(input("Ingresar multiplicando: "))
+    sustraendo = float(input("Ingresar multiplicando: "))
+    resultado = operacion.Restar(minuendo, sustraendo)
+    print(f"Resta -> {minuendo} - {sustraendo} = {resultado}")
